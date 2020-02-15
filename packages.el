4dd15f8255122e2f79cf7f876734b54471bb9aa6
Wang Liao <lwlw1988@gmail.com>
2020-02-15 21:51:31 +0800

    test

--- packages.el
+++ packages.el
@@ -1,7 +1,7 @@
 ;; -*- no-byte-compile: t; -*-
 ;;; ~/.doom.d/packages.el
 
-;;; Examples:
+;;; Examples
 ;; (package! some-package)
 ;; (package! another-package :recipe (:fetcher github :repo "username/repo"))
 ;; (package! builtin-package :disable t)
